ed2857b4e42361bef4923df38e5491512d2348ca
Gregory <sorax.js@gmail.com>
2020-09-05 23:55:37 +0300

    Added style change when To do is executed

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 
 export default ({ item, completedItem }) => {
+
+    const optionsTitle = cn({
+        "title-text": true,
+        "title-text-checked": item.complete
+    });
+
     return (
       <div className="todo__task">
           <label>
               <input type="checkbox" className="todo__checkbox" onChange={completedItem(item.id)}/>
               <span className="fake">check</span>
-              <span className="title-text title-text-checked">{item.title}</span>
+              <span className={optionsTitle}>{item.title}</span>
           </label>
       </div>
     );
